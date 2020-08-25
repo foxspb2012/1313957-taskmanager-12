9a191a8972ee966421981edc8ffb8c8505ec5881
Евгений Потапов <pea@uiex.com>
2020-08-25 08:11:16 +0300

    Выполнено задание Учебный проект: шаблонизируй это

--- src/view/task.js
+++ src/view/task.js
@@ -1,46 +1,68 @@
-export const createTaskTemplate = () => {
-  return (
-    `<article class="card card--black">
-      <div class="card__form">
-        <div class="card__inner">
-          <div class="card__control">
-            <button type="button" class="card__btn card__btn--edit">
-              edit
-            </button>
-            <button type="button" class="card__btn card__btn--archive">
-              archive
-            </button>
-            <button
-              type="button"
-              class="card__btn card__btn--favorites"
-            >
-              favorites
-            </button>
-          </div>
+import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from "../utils.js";
 
-          <div class="card__color-bar">
-            <svg class="card__color-bar-wave" width="100%" height="10">
-              <use xlink:href="#wave"></use>
-            </svg>
-          </div>
+export const createTaskTemplate = (task) => {
+  const {color, description, dueDate, repeating, isArchive, isFavorite} = task;
 
-          <div class="card__textarea-wrap">
-            <p class="card__text">Example default task with default color.</p>
-          </div>
+  const date = dueDate !== null
+    ? humanizeTaskDueDate(dueDate)
+    : ``;
 
-          <div class="card__settings">
-            <div class="card__details">
-              <div class="card__dates">
-                <div class="card__date-deadline">
-                  <p class="card__input-deadline-wrap">
-                    <span class="card__date">23 September</span>
-                  </p>
-                </div>
+  const deadlineClassName = isTaskExpired(dueDate)
+    ? `card--deadline`
+    : ``;
+
+  const repeatClassName = isTaskRepeating(repeating)
+    ? `card--repeat`
+    : ``;
+
+  const archiveClassName = isArchive
+    ? `card__btn--archive card__btn--disabled`
+    : `card__btn--archive`;
+
+  const favoriteClassName = isFavorite
+    ? `card__btn--favorites card__btn--disabled`
+    : `card__btn--favorites`;
+
+  return `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
+    <div class="card__form">
+      <div class="card__inner">
+        <div class="card__control">
+          <button type="button" class="card__btn card__btn--edit">
+            edit
+          </button>
+          <button type="button" class="card__btn ${archiveClassName}">
+            archive
+          </button>
+          <button
+            type="button"
+            class="card__btn ${favoriteClassName}"
+          >
+            favorites
+          </button>
+        </div>
+
+        <div class="card__color-bar">
+          <svg class="card__color-bar-wave" width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+        </div>
+
+        <div class="card__textarea-wrap">
+          <p class="card__text">${description}</p>
+        </div>
+
+        <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              <div class="card__date-deadline">
+                <p class="card__input-deadline-wrap">
+                <span class="card__date">${date}</span>
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </article>`
-  );
+    </div>
+  </article>`;
 };
